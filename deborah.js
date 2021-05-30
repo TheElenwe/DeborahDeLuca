96b5f3887fd8b33f54f28ab9d52b88a2a027fdb6
unknown <you@example.com>
2021-05-30 15:55:24 +0300

    second commit'

---
+++ deborah.js
@@ -0,0 +1,11 @@
+  document.querySelector('button').addEventListener('click', function (){
+    let img = document.querySelector('img').src='https://pbs.twimg.com/profile_images/1335228206081843205/vtngoVm9_400x400.jpg'
+        //img.style.display='block';
+    });
+
+  document.querySelector('#eksodos').addEventListener('click',function (){
+     let img =document.querySelector('img').src=''
+     //img.style.display="none";
+  })
+
+

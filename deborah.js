582bdb71ea9b0ff85ebcf0a8521c397875234ad7
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2021-06-03 00:01:38 +0300

    Update deborah.js

--- deborah.js
+++ deborah.js
@@ -1,11 +1,18 @@
-  document.querySelector('button').addEventListener('click', function (){
-    let img = document.querySelector('img').src='https://pbs.twimg.com/profile_images/1335228206081843205/vtngoVm9_400x400.jpg'
+    document.querySelector('.information').addEventListener('click', function (a){
+    let img = document.querySelector('#image').src=`https://www.amsterdam-dance-event.nl/uploads/images/artists-speakers/_520x650_crop_center-center_none/23376092_10155481309824760_1410750844656635508_n_154711.jpg  ` 
+    img=document.querySelector('#image').style.visibility='visible';
+    document.querySelector('p').innerHTML="Deborah De Luca (* 1980 in Naples, Italy) is an Italian DJ and techno producer."
+       
+      
         //img.style.display='block';
-    });
+    })
 
-  document.querySelector('#eksodos').addEventListener('click',function (){
-     let img =document.querySelector('img').src=''
+  document.querySelector('button').addEventListener('click',function (){
+     let img =document.querySelector('#image').src=''
+     document.querySelector('p').innerHTML=''
+    
      //img.style.display="none";
   })
 
 
+  

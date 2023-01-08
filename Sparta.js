4a81f0ad084e8f5f2136dadd3e254982432d817d
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2023-01-08 21:27:44 +0200

    Update Sparta.js

--- Sparta.js
+++ Sparta.js
@@ -1,31 +1,53 @@
-<React.Fragment>
-      <div className={classes.imagesContainer}>
-        <div className={classes.imageContainer}>
-          <img className={classes.imagePetra} src={petra} alt="Petra" />
+import React from "react";
+// import spartaLogo from "../assets/spartaLogo.jpg";
+import spartaVideo from "../assets/spartaVideo.mp4";
+import petra from "../assets/petra.png";
+import petraSecond from "../assets/petraSeocond";
+import spartaLogo from "../assets/spartaLogo.jpg";
+import classes from "./Home.module.css";
 
-          <img
-            className={classes.spartaLogo}
-            src={spartaLogo}
-            alt="sparta logo"
-          />
-  
-          <h1 className={classes.imageHeaderText}>
-            Im Herzen von Dachau
-            {/* <br></br><span className={classes.yearText}>Seit 2013</span> <br></br> */}
-          
-          </h1>
-            {/* <a href="/" target="_blank" className={classes.icons}>
-        <i className="fa fa-facebook-square" ></i>
-      </a>
-      &nbsp; &nbsp;
-      <a href="/" target="_blank" className={classes.icons}>
-        <i className="fa fa-instagram"></i>
-      </a> */}
-    
-         
+const Home = (props) => {
+  const styles = {
+    body: {
+      overflowX: "hidden",
+    },
+  };
+  return (
+    <React.Fragment>
+      <div style={styles.body}>
+        <div className={classes.imagesContainer}>
+          <div className={classes.imageContainer}>
+            <img className={classes.imagePetra} src={petra} alt="Petra" />
+
+            <img
+              className={classes.spartaLogo}
+              src={spartaLogo}
+              alt="sparta logo"
+            />
+
+            <div>
+              <h1 className={classes.textHeaderContainer}>
+                <span className={classes.headerChange}>
+                  {" "}
+                  Im Herzen von Dachau
+                </span>
+                <br></br>
+                <span className={classes.yearText}>Seit 2013</span> <br></br>
+                <a href="/" target="_blank" className={classes.icons}>
+                  <i className="fa fa-facebook-square"></i>
+                </a>
+                &nbsp; &nbsp;
+                <a href="/" target="_blank" className={classes.icons}>
+                  <i className="fa fa-instagram"></i>
+                </a>
+              </h1>
+            </div>
+          </div>
+
+          <video src={spartaVideo} muted autoPlay controls={false} loop />
         </div>
-        <video src={spartaVideo} muted autoPlay controls={false} loop />
       </div>
-
-    
     </React.Fragment>
+  );
+};
+export default Home;

ea53914020f85a4dcc956e273cc2df1cec245a5a
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2023-01-14 13:29:29 +0200

    Update Sparta.js

--- Sparta.js
+++ Sparta.js
@@ -10,6 +10,7 @@ import imageLeftRightFirst from "../assets/imageLeftRightFirst.jpg";
 import imageLeftRightSecond from "../assets/imageLeftRightSecond.jpg";
 import transformImageFirst from "../assets/transformImageFirst.jpg";
 import transformImageSecondWein from "../assets/transformImageSecondWein.jpg";
+import tsoukrisa from '../assets/tsoukrisma.jpg'
 import classes from "./Home.module.css";
 
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
@@ -41,29 +42,46 @@ const Home = (props) => {
   const imageTransform1Ref = useRef(null);
   const imageTransform2Ref = useRef(null);
   const h2TransformRef = useRef(null);
+  const h2TransformRefWein = useRef(null);
+  const petraBackgroundThirdRef = useRef(null);
 
   useEffect(() => {
     let last_pos = window.pageYOffset;
     const handleScroll = () => {
-      let current_pos = window.pageYOffset;
-      if (current_pos > last_pos) {
-        imageTransform1Ref.current.style.transform = `translateY(100px)`;
-        imageTransform2Ref.current.style.transform = `translateY(-100px)`;
-        h2TransformRef.current.style.transform = `translateY(200px)`;
-      } else {
-        imageTransform1Ref.current.style.transform = `translateY(-100px)`;
-        imageTransform2Ref.current.style.transform = `translateY(100px)`;
-        h2TransformRef.current.style.transform = `translateY(-50px)`;
+      const petraBackgroundThird = petraBackgroundThirdRef.current;
+      const petraBackgroundThirdRect =
+        petraBackgroundThird.getBoundingClientRect();
+      const petraBackgroundThirdTop = petraBackgroundThirdRect.top;
+      const petraBackgroundThirdBottom = petraBackgroundThirdRect.bottom;
+
+      if (petraBackgroundThirdTop <= 0 && petraBackgroundThirdBottom >= 0) {
+        let current_pos = window.scrollY;
+        if (current_pos > last_pos) {
+          imageTransform1Ref.current.style.transform = `translateY(100px)`;
+          imageTransform2Ref.current.style.transform = `translateY(-100px)`;
+          h2TransformRef.current.style.transform = `translateY(50px) translateX(-80px)`;
+          h2TransformRefWein.current.style.transform = `translateY(-130px) translateX(-50px)`;
+        } else {
+          imageTransform1Ref.current.style.transform = `translateY(-100px)`;
+          imageTransform2Ref.current.style.transform = `translateY(100px)`;
+          h2TransformRef.current.style.transform = `translateY(-100px) translateX(-80px)`;
+          h2TransformRefWein.current.style.transform = `translateY(70px) translateX(-40px) `;
+        }
+        last_pos = current_pos;
+        console.log("work scrolling");
       }
-      console.log('scroll')
-      last_pos = current_pos;
     };
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-    
-  }, [imageTransform1Ref, imageTransform2Ref, h2TransformRef]);
-
-
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [
+    imageTransform1Ref,
+    imageTransform2Ref,
+    h2TransformRef,
+    h2TransformRefWein,
+    petraBackgroundThirdRef,
+  ]);
   // let img1Ref = React.createRef();
   // let img2Ref = React.createRef();
 
@@ -121,7 +139,7 @@ const Home = (props) => {
             alt="sparta logo"
           />
           <div>
-            <h1   className={classes.textHeaderContainer}>
+            <h1 className={classes.textHeaderContainer}>
               <span className={classes.headerChange}>
                 {" "}
                 Im Herzen von Dachau
@@ -198,32 +216,51 @@ const Home = (props) => {
       <div className={classes.imagesContainer}>
         <div className={classes.transformImage}>
           <div className={classes.transformImages}>
+            <a href="/">
+              <img
+                ref={imageTransform1Ref}
+                src={transformImageFirst}
+                className={`${classes.imgTransform} ${classes.imgZoom}`}
+                alt="food"
+                style={{ width: "32rem", height: "50rem" }}
+              />
+
+              <h2
+                ref={h2TransformRef}
+                className={classes.centerH2TransformImage}
+              >
+                Speisen
+              </h2>
+            </a>
+          </div>
+          <a href="/">
             <img
-              ref={imageTransform1Ref}
-              src={transformImageFirst}
+              ref={imageTransform2Ref}
+              src={transformImageSecondWein}
               className={classes.imgTransform}
-              alt="food"
+              alt="foodx"
               style={{ width: "32rem", height: "50rem" }}
             />
-
-            <h2 ref={ h2TransformRef} className={classes.centerH2TransformImage}>Spein</h2>
-          </div>
-
-          <img
-            ref={imageTransform2Ref}
-            src={transformImageSecondWein}
-            className={classes.imgTransform}
-            alt="foodx"
-            style={{ width: "32rem", height: "50rem" }}
-          />
+            <h2
+              ref={h2TransformRefWein}
+              className={classes.centerH2TransformImageWein}
+            >
+              Wein
+            </h2>
+          </a>
         </div>
         <img
+          ref={petraBackgroundThirdRef}
           src={petra}
           className={classes.petraBackgroundThird}
           alt="petraThird"
         />
       </div>
+      <div className={classes.tsoukrismaWhiteBackground}> 
+
+      </div>
     </React.Fragment>
   );
 };
+
 export default Home;

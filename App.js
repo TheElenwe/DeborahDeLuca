8082f6e762d2f3ca1062e5c01c283da7a3566e31
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2023-01-21 00:03:59 +0200

    SPARTAAAA

---
+++ App.js
@@ -0,0 +1,21 @@
+import Home from './components/Home';
+import Meta from './components/Meta'
+import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
+import React from 'react';
+
+
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Meta />
+       <Home/>
+       </React.Fragment>
+    
+     
+  );
+}
+
+export default App;
